34e644a3873b31f68cd4ca937126789acdea4322
Rajwardhann <75781405+Rajwardhann@users.noreply.github.com>
2021-02-26 12:22:14 +0530

    Add files via upload

--- projecttemplate-31-main/sketch.js
+++ projecttemplate-31-main/sketch.js
@@ -6,8 +6,9 @@ const World = Matter.World;
 var dropdown = [];
 function preload(){
     //boyimg = loadImage("bboy.png");
-    thunder1img = loadImage("thunder1.png")
-    thunder2img = loadImage("thunder2.png")
+    thunder1img = loadImage("thunder1.png");
+    thunder2img = loadImage("thunder2.png");
+
 }
 
 function setup(){
@@ -28,6 +29,23 @@ function setup(){
 
 function draw(){
     background("black");
+    drawSprites();
+    if(frameCount%80 === 0)
+    {
+        rand = Math.round(random(1,2));
+        var thunder = createSprite(random(10,370), random(10,30),10,10);
+        switch(rand)
+        {
+            case 1: thunder.addImage(thunder1img);
+            break;
+            case 2: thunder.addImage(thunder2img);
+            break;
+            
+        }
+
+        thunder.scale = random(0.3,0.6);
+    }
+
     maxdrops = 100;
     if(frameCount%20==0){
         for(var i=0; i < maxdrops; i++)
